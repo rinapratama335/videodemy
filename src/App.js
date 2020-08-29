4c759267eb33356fba32a9d5a81b48cd18bcea5c
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-08-30 03:03:54 +0700

    membuat halaman konten utama (#1)
    Co-authored-by: irwantowibowotech <irwantowibowotech@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./css/props.css";
 import "./css/App.css";
 import Header from "./screens/header";
 import Sidebar from "./screens/sidebar";
+import HomePage from "./screens/home";
 
 function App() {
   return (
     <div className="App flex">
       <Sidebar />
+      <div className="app-content">
+        <HomePage />
+      </div>
     </div>
   );
 }
---
+++ src/screens/home.js
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+
+import Course1 from "../ui/illustrator.jpg";
+import Course2 from "../ui/course1.jpg";
+
+function HomePage() {
+  const [popularCourse, setPopularCourse] = useState([
+    {
+      ID: 1,
+      title:
+        "Learning How to Create Beautiful Scenes in Illustrator in 60 minutes",
+      tutor: {
+        ID: 1,
+        name: "Rina Pratama",
+        username: "@rinapratama",
+        dp: "http://placeimg.com/100/100/people?tutor-" + 1,
+      },
+      duration: "120 min",
+      poster: Course1,
+    },
+    {
+      ID: 2,
+      title: "How to create social media app with ReactJS and NodeJS",
+      tutor: {
+        ID: 2,
+        name: "Mahmud Mantab",
+        username: "@mahmudmantab",
+        dp: "http://placeimg.com/100/100/people?tutor-" + 2,
+      },
+      duration: "89 min",
+      poster: Course2,
+    },
+  ]);
+
+  const [topTutors, setTopTutors] = useState([
+    {
+      ID: 1,
+      name: "Rina Pratama",
+      username: "@rinapratama",
+      dp: "http://placeimg.com/100/100/people?tutors-" + 1,
+    },
+    {
+      ID: 1,
+      name: "Rina Pratama",
+      username: "@rinapratama",
+      dp: "http://placeimg.com/100/100/people?tutors-" + 1,
+    },
+    {
+      ID: 3,
+      name: "Rina Pratama",
+      username: "@rinapratama",
+      dp: "http://placeimg.com/100/100/people?tutors-" + 3,
+    },
+    {
+      ID: 4,
+      name: "Rina Pratama",
+      username: "@rinapratama",
+      dp: "http://placeimg.com/100/100/people?tutors-" + 4,
+    },
+    {
+      ID: 5,
+      name: "Rina Pratama",
+      username: "@rinapratama",
+      dp: "http://placeimg.com/100/100/people?tutors-" + 5,
+    },
+    {
+      ID: 6,
+      name: "Rina Pratama",
+      username: "@rinapratama",
+      dp: "http://placeimg.com/100/100/people?tutors-" + 6,
+    },
+  ]);
+
+  //Live Tutors List
+  var tutorList = [];
+  for (let i = 0; i < 8; i++) {
+    tutorList.push(
+      <button className="tutor rel" key={"tutor-live-" + i}>
+        <img src={"http://placeimg.com/100/100/people?" + i} className="bl" />
+      </button>
+    );
+  }
+
+  //Courses List
+  var courseList = [];
+  for (let i = 0; i < popularCourse.length; i++) {
+    courseList.push(
+      <a className="course rel" key={"popular-course-" + i}>
+        <div
+          className="block rel"
+          style={{
+            background:
+              "#e2e2e2 url(" + popularCourse[i].poster + ") no-repeat center",
+          }}
+        >
+          <div className="user abs aic flex">
+            <div className="pic">
+              <img src={popularCourse[i].tutor.dp} className="bl" />
+            </div>
+
+            <div className="meta rel">
+              <h2 className="s15 name fontb cfff">
+                {popularCourse[i].tutor.name}
+              </h2>
+              <h2 className="s13 uname fontn cfff">
+                {popularCourse[i].tutor.username}
+              </h2>
+            </div>
+          </div>
+
+          <div className="dura abs">
+            <h2 className="s13 name fontb cfff">{popularCourse[i].duration}</h2>
+          </div>
+
+          <div className="course-title abs">
+            <h2 className="s15 name fontb cfff">{popularCourse[i].title}</h2>
+          </div>
+        </div>
+      </a>
+    );
+  }
+
+  //Top Tutors List
+  var topTutorList = [];
+  for (let i = 0; i < topTutors.length; i++) {
+    topTutorList.push(
+      <a className="user-block noul rel" key={"top-tutors-" + i}>
+        <div className="user aic flex">
+          <div className="pic">
+            <img src={topTutors[i].dp} className="bl" />
+          </div>
+
+          <div className="meta rel">
+            <h2 className="s15 name fontb c333">{topTutors[i].name}</h2>
+            <h2 className="s13 uname fontn c333">{topTutors[i].username}</h2>
+          </div>
+        </div>
+      </a>
+    );
+  }
+
+  return (
+    <div className="home-page rel">
+      {/* Tutors Live Now */}
+      <div className="section rel">
+        <h2 className="title s24 fontb">
+          Streaming <span className="fontn">Now</span>
+        </h2>
+
+        <div className="tutors rel flex">{tutorList}</div>
+      </div>
+
+      {/* Popular Courses */}
+      <div className="section section-b rel">
+        <h2 className="title s24 fontb">
+          Popular <span className="fontn">This Week</span>
+        </h2>
+
+        <div className="courses rel flex">{courseList}</div>
+      </div>
+
+      {/* Top Tutors */}
+      <div className="section rel">
+        <h2 className="title s24 fontb">
+          Top <span className="fontn">Tutors</span>
+        </h2>
+
+        <div className="top-tutors rel flex">{topTutorList}</div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
